c1bbeff7cdfaf28ffd0f5a1e450ebdcd6c4ea5d2
Matthew Puentes <matt.puentes@gmail.com>
2018-11-11 18:19:16 -0500

    basic sound functionality

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function setup() {
-
+function preload() {
+    song = loadSound('Song.mp3');
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80);
+function setup() {
+    createCanvas(1000, 1000);
+    song.loop(); // song is ready to play during setup() because it was loaded during preload
+    background(0,255,0);
+}
+
+function mousePressed() {
+    if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+        song.stop();
+        background(255,0,0);
+    } else {
+        song.play();
+        background(0,255,0);
+    }
 }
